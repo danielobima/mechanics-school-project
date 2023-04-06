20063f163272499f950e90624a89ddaa974c6dd3
Anthony Nambiro Peter Mamati <121312689+nambiropeter@users.noreply.github.com>
2023-04-06 22:39:20 +0300

    trial

--- backend/sql_statements/mechanics_querry.sql
+++ backend/sql_statements/mechanics_querry.sql
@@ -1,3 +1,4 @@
-SELECT DISTINCTROW Mechanics.MechanicName, Sum(SpareParts.Cost) AS Earned
+SELECT DISTINCTROW Mechanics.MechanicName, Sum(SpareParts.Cost)
+ AS Earned
 FROM (Mechanics INNER JOIN JobCards ON Mechanics.[MechanicID] = JobCards.[MechanicID]) INNER JOIN (SpareParts INNER JOIN JobSpareParts ON SpareParts.[SparePartID] = JobSpareParts.[SparePartID]) ON JobCards.[JobID] = JobSpareParts.[JobID]
 GROUP BY Mechanics.MechanicName, JobCards.JobID;
