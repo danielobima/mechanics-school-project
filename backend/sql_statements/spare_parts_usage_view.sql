1c4d601d2ed79c304216bb5089e3c31ee1d76432
danielobima <demannman67@gmail.com>
2023-05-15 04:33:04 +0300

    skipidi dipidi doop

---
+++ backend/sql_statements/spare_parts_usage_view.sql
@@ -0,0 +1,7 @@
+CREATE VIEW spare_parts_usage_view AS
+SELECT sp.part_name, SUM(jp.quantity_used) AS total_quantity_used
+FROM job_parts jp
+INNER JOIN jobs j ON jp.job_id = j.job_id
+INNER JOIN spare_parts sp ON jp.part_id = sp.part_id
+GROUP BY sp.part_name
+ORDER BY total_quantity_used DESC
---
+++ backend/sql_statements/vehicles_view.sql
@@ -0,0 +1,4 @@
+CREATE VIEW vehicles_view AS
+SELECT v.*, c.customer_name, c.customer_phone
+FROM vehicles v
+LEFT JOIN customers c ON v.customer_id = c.customer_id
